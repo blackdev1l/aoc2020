108678c90aae2d00bd52e6210302a495b79ecd62
Cristian Achille <c.achille@iungo.it>
2020-12-14 20:16:28 +0100

    shitty solution for part 2

--- lib/aoc20/day1.ex
+++ lib/aoc20/day1.ex
@@ -1,42 +1,86 @@
 require IO
 require Enum
+
 defmodule Aoc20.Day1 do
   @moduledoc """
-  Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+
+   --- part 1 ---
+   Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
   Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
   For example, suppose your expense report contained the following:
 
-    1721
-    979
-    366
-     9
-    675
-    1456
+   1721
+   979
+   366
+    9
+   675
+   1456
 
   In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
   Permuto ogni elemento della lista finchè non troovo la coppai che sommata fa 2020
   per cui :
-    per ogni elemento in lista
-      sommo quell'elemento per ogni altro elemento nella lista
-        se trovo la combinazione ritorno e moltiplcio
-        altrimenti rimuovo quell'elemento e ripeto
-   """
+   per ogni elemento in lista
+     sommo quell'elemento per ogni altro elemento nella lista
+       se trovo la combinazione ritorno e moltiplcio
+       altrimenti rimuovo quell'elemento e ripeto
+
+  --- Part 2 ---
+
+   The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation.
+   They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+   Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+   In your expense report, what is the product of the three entries that sum to 2020?
+
+   prendo la prima
+  """
 
   def hello do
     "world"
   end
 
   def findEntries(list) do
-    Enum.flat_map(list, fn x -> [x *findSum(list,x)] end)
+    Enum.flat_map(list, fn x -> [x * findSum(list, x)] end)
     |> Enum.filter(fn x -> x != 0 end)
-    |> List.first
+    |> List.first()
   end
 
   def findSum(list, elem) do
-    Enum.find(list,0, fn x ->
+    Enum.find(list, 0, fn x ->
       if elem + x == 2020 do
         true
+      end
+    end)
+  end
+
+  def find_entries(list) do
+    Enum.reduce_while(list, 0, fn x, _x_acc ->
+      rs =
+        Enum.reduce_while(list, 0, fn y, _acc ->
+          result =
+            Enum.reduce_while(list, 0, fn z, _acc ->
+              if x + y + z == 2020 do
+                IO.puts("#{x} #{y} #{z} ")
+                IO.puts("#{x * y * z}")
+                {:halt, x * y * z}
+              else
+                {:cont, 0}
+              end
+            end)
+
+          if result != 0 do
+            {:halt, result}
+          else
+            {:cont, 0}
+          end
+        end)
+
+      if rs != 0 do
+        {:halt, rs}
+      else
+        {:cont, 0}
       end
     end)
   end
--- test/aoc20/day1_test.exs
+++ test/aoc20/day1_test.exs
@@ -17,14 +17,13 @@ defmodule Aoc20.Day1Test do
   end
 
   test "part one", %{reports: reports} do
-    assert "world" == Day1.hello()
-    assert 514579 == Day1.findEntries([1721,979,366,299,675,1456])
-    assert 5 == Day1.findSum([5,100,12,340],2015)
-    assert 902451 == Day1.findEntries(reports)
+    assert 514_579 == Day1.findEntries([1721, 979, 366, 299, 675, 1456])
+    assert 5 == Day1.findSum([5, 100, 12, 340], 2015)
+    assert 902_451 == Day1.findEntries(reports)
   end
-
 
   test "part two", %{reports: reports} do
+    assert 241_861_950 = Day1.find_entries([1721, 979, 366, 299, 675, 1456])
+    assert 85_555_470 == Day1.find_entries(reports)
   end
 end
-
