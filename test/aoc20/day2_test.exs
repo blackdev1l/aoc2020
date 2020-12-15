3d3e4c4b83886b816092bc548240b8c4c48077b8
Cristian Achille <c.achille@iungo.it>
2020-12-15 09:41:52 +0100

    day 2 part 1 done

---
+++ test/aoc20/day2_test.exs
@@ -0,0 +1,23 @@
+defmodule Aoc20.Day2Test do
+  use ExUnit.Case
+  doctest Aoc20.Day2
+
+  alias Aoc20.Day2
+
+  @data_dir Path.join([Application.app_dir(:aoc20), "priv", "day2"])
+
+  test "part one" do
+    input = File.stream!(Path.join(@data_dir, "input"))
+    combination = Day2.parse("6-7 w: wkjnmww")
+    assert "6" == combination["min"]
+    assert "7" == combination["max"]
+    assert "w" == combination["letter"]
+    assert "wkjnmww" == combination["password"]
+    assert 2 == Day2.read(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"])
+
+    assert 564 == Day2.read(input)
+  end
+
+  test "part two" do
+  end
+end
